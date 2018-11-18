39c9f06573d7e8bc0241e656ee1215fd0c6cf87a
ananth2 <ananthram.p@tresbu.com>
2018-11-18 09:43:39 +0530

    new branch commiting

---
+++ src/test/java/tests/Test3.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.testng.annotations.BeforeTest;
+
+public class Test3 {
+	
+	
+	@BeforeTest
+	
+	
+	public void test1(){
+		
+		System.out.println("hello1 new testt");
+		
+	}
+
+}
